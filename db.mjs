71ef1f6490aefd41b18574d6e91ff7e049aa4ff5
Paul Ulerick <p.ulerick17@gmail.com>
2022-10-18 19:38:51 -0400

    Made js file that creates the database connection, wrote the logic for the email feature that sends the reminder email a week before the reminders date, removed some excess code in the main js file

---
+++ db.mjs
@@ -0,0 +1,10 @@
+const mysql = require("mysql2");
+
+// Create database connection
+export const db = mysql.createConnection({
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
+});
+  
--- schedule.js
+++ schedule.js
@@ -1,58 +1,61 @@
-const cron = require('node-cron')
-
-// Creates db connection
-const db = mysql.createConnection({
-    host: process.env.HOST,
-    user: process.env.USER,
-    password: process.env.PASSWORD,
-    database: process.env.DATABASE,
-  });
+const res = require("express/lib/response");
+const cron = require("node-cron");
+const mysql = require("mysql2");
+const express = require("express");
+const { send, sendStatus } = require("express/lib/response");
+require("dotenv").config();
+import { db } from "./db.mjs"
 
 // Connect to database
 db.connect((err) => {
     if (err) {
-      throw err;
+        throw err;
     }
     console.log("MySql has connected");
 });
 
 // Schedule tasks to be run on the server.
-cron.schedule('0 0 * * *', function() {
+cron.schedule("0 0 * * *", function () {
     let date = new Date();
-	let current_date = (date.getFullYear+"-"+date.getMonth()+"-"+ date.getDate() + 7)
-    let user_date;
-    date.get
+    let next_week = date.getFullYear + "-" + date.getMonth() + "-" + date.getDate() + 7;
 
-    const search = "SELECT * FROM reminders where id = ?";
-    const reminder_search = mysql.format(search, [new_id[0]]);
+    const search = "SELECT * FROM reminders where date = ?";
+    const reminder_search = mysql.format(search, [next_week]);
 
-    db.query(reminder_search, async (err, res) => {
-
-    })
-
-    if (x + 7 == date.getDate() && x == date.getMonth()) {
-
-    }
+    // Selects reminders where the date is a week away
+    db.query(reminder_search, (err, res) => {
+        if (err) throw err;
+        // iterates through the reminders
+        for (let x = 0; x > res.length; x++) {
+            var reminderDate = JSON.stringify(res[x].date);
+            var reminderTime = JSON.stringify(res[x].time);
+            var reminderContent = JSON.stringify(res[x].reminder);
+            var remidnerEmail = JSON.stringify(res[x].email)
+            // Calls function that sends the reminder email to the email corresponding to the reminder
+            main(remidnerEmail, reminderContent, reminderTime, reminderDate)
+        }
+    });
 });
 
-async function main(reciever) {
+// Main function that sends reminder emails to the user who made them
+// Called a week before every reminder's date 
+async function main(email, reminder, time, date) {
     // creates the email object that will be sending reminder emails
     let transporter = nodemailer.createTransport({
-      service: 'gmail',
-      auth: {
-        user: process.env.EMAIL_SERVICE,
-        pass: process.env.EMAIL_PASS
-      }
+        service: "gmail",
+        auth: {
+            user: process.env.EMAIL_SERVICE,
+            pass: process.env.EMAIL_PASS,
+        },
     });
-  
+
     // send mail with defined transport object
     let email_details = await transporter.sendMail({
-      from: process.env.EMAIL_SERVICE, // sender address
-      to: reciever, // list of receivers
-      subject: "Hello", // Subject line
-      text: "Hello world?", // plain text body
-      html: "<b>Hello world?</b>", // html body
+        from: process.env.EMAIL_SERVICE, // sender address
+        to: email, // list of receivers
+        subject: "reminder", // Subject line
+        text: `Don't forget, you have a reminder: ${reminder} on ${date} at ${time}, Thank you for using our service and have a great day!` // plain text body
     });
-  
-    console.log('Email has been sent')
-  }
+
+    console.log("Email has been sent");
+}
